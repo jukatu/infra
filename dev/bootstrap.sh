c562b292c2e195c61b02c6c6893bd630c93ee3d5
sukulu <sukulu@noreply.net>
2020-12-11 17:01:51 -0500

    Working on dev bootstrap

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -4,14 +4,14 @@ GITHUB_USER=$1
 GITHUB_TOKEN=$2
 CLUSTER=dev
 
-kind create cluster --name $CLUSTER --config cluster.yaml
+# kind create cluster --name $CLUSTER --config cluster.yaml
 
-kubectl cluster-info --context kind-$CLUSTER
+# kubectl cluster-info --context kind-$CLUSTER
 
-flux bootstrap github \
-  --owner=$GITHUB_USER \
-  --repository=infra \
-  --branch=main \
-  --path=dev-cluster \
-  --context kind-$CLUSTER \
-  --personal
+# flux bootstrap github \
+#   --owner=$GITHUB_USER \
+#   --repository=infra \
+#   --branch=main \
+#   --path=dev-cluster \
+#   --context kind-$CLUSTER \
+#   --personal
