8e4f37319f305d62cd534b3513cd84929ae0c870
sukulu <sukulu@noreply.net>
2020-12-11 16:54:04 -0500

    added dev

---
+++ dev/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GITHUB_USER=$1
+GITHUB_TOKEN=$2
+CLUSTER=dev
+
+kind create cluster --name $CLUSTER --config cluster.yaml
+
+kubectl cluster-info --context kind-$CLUSTER
+
+flux bootstrap github \
+  --owner=$GITHUB_USER \
+  --repository=infra \
+  --branch=main \
+  --path=dev-cluster \
+  --context kind-$CLUSTER \
+  --personal
